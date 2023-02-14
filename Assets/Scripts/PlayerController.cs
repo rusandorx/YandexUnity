d18972bde401815343382380468c81636249071d
Rusandor <ruslanpalkin44@gmail.com>
2023-02-14 23:10:32 +0300

    3-2/complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private readonly float _swingVelocity = 100f;
+    private readonly float _swingVelocity = 130f;
 
-    private bool _attached = false;
+    private bool _attached;
 
     private HingeJoint2D _lastRopeSegment;
     private DistanceJoint2D _rope;
     private Rigidbody2D _rigidbody;
+
+    private CapsuleCollider2D _collider;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CapsuleCollider2D>();
+    }
 
     void Attach(GameObject ropeGameObject)
     {
         if (_attached)
             return;
 
+        transform.position = new Vector2(ropeGameObject.transform.position.x,
+            GetMinGameObjectPoint<CapsuleCollider2D>(ropeGameObject.transform).y -
+            _collider.size.y / 2);
+
         _rope = ropeGameObject.transform.parent.gameObject.GetComponent<DistanceJoint2D>();
         _lastRopeSegment = ropeGameObject.GetComponent<HingeJoint2D>();
+        transform.rotation = _lastRopeSegment.transform.rotation;
 
         _lastRopeSegment.connectedBody = _rigidbody;
         _rope.connectedBody = _rigidbody;
+        _lastRopeSegment.connectedAnchor = new Vector2(0, _collider.size.y / 2);
+        _rope.connectedAnchor = new Vector2(0, _collider.size.y / 2);
         _lastRopeSegment.enabled = true;
         _rope.enabled = true;
         _attached = true;
-
-        transform.position = new Vector2(ropeGameObject.transform.position.x,
-            ropeGameObject.GetComponent<CapsuleCollider2D>().bounds.min.y - GetComponent<CapsuleCollider2D>().size.y / 2);
     }
 
     void Detach()
@@ -36,26 +48,24 @@ public class PlayerController : MonoBehaviour
         _lastRopeSegment.connectedBody = null;
         _rope.connectedBody = null;
         _lastRopeSegment.enabled = false;
-        _rope.enabled = false;
+        Rigidbody2D ropeSegmentRigidbody = _lastRopeSegment.transform.GetComponent<Rigidbody2D>();
+        _rope.connectedBody = ropeSegmentRigidbody;
+        _rope.connectedAnchor = Vector2.zero;
         _attached = false;
     }
 
-    void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
+    static Vector2 GetMinGameObjectPoint<T>(Transform target) where T : Collider2D =>
+        target.GetComponent<T>().bounds.min;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) 
             Detach();
-        }
 
         _rigidbody.AddForce(new Vector2(Input.GetAxis("Horizontal") * _swingVelocity * Time.deltaTime, 0));
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerStay2D(Collider2D col)
     {
         string colTag = col.gameObject.tag;
         switch (colTag)
