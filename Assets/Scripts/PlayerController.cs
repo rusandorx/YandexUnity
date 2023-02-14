708c0627881b13296e7a6a6c2b8cc87c9dd7cd38
Rusandor <ruslanpalkin44@gmail.com>
2023-02-14 20:17:04 +0300

    2-3/steps in right direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private HingeJoint2D _lastRopeSegment;
-    [SerializeField] private DistanceJoint2D _rope;
-    private readonly float _velocity = 100f; 
+    private readonly float _swingVelocity = 100f;
+
+    private bool _attached = false;
+
+    private HingeJoint2D _lastRopeSegment;
+    private DistanceJoint2D _rope;
     private Rigidbody2D _rigidbody;
+
+    void Attach(GameObject ropeGameObject)
+    {
+        if (_attached)
+            return;
+
+        _rope = ropeGameObject.transform.parent.gameObject.GetComponent<DistanceJoint2D>();
+        _lastRopeSegment = ropeGameObject.GetComponent<HingeJoint2D>();
+
+        _lastRopeSegment.connectedBody = _rigidbody;
+        _rope.connectedBody = _rigidbody;
+        _lastRopeSegment.enabled = true;
+        _rope.enabled = true;
+        _attached = true;
+
+        transform.position = new Vector2(ropeGameObject.transform.position.x,
+            ropeGameObject.GetComponent<CapsuleCollider2D>().bounds.min.y - GetComponent<CapsuleCollider2D>().size.y / 2);
+    }
 
     void Detach()
     {
+        if (!_attached)
+            return;
+
         _lastRopeSegment.connectedBody = null;
         _rope.connectedBody = null;
         _lastRopeSegment.enabled = false;
         _rope.enabled = false;
+        _attached = false;
     }
 
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
-    
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Detach();
         }
-        _rigidbody.AddForce(new Vector2(Input.GetAxis("Horizontal") * _velocity * Time.deltaTime, 0));
+
+        _rigidbody.AddForce(new Vector2(Input.GetAxis("Horizontal") * _swingVelocity * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        string colTag = col.gameObject.tag;
+        switch (colTag)
+        {
+            case "Rope":
+                Attach(col.gameObject);
+                break;
+        }
     }
 }
